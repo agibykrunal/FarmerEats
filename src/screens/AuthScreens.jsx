f188871967a4fa918d8b347d96fd302524f733ff
Krunal <nsundersia@gmail.com>
2026-04-28 17:16:14 +0530

    Update AuthScreens.jsx

--- src/screens/AuthScreens.jsx
+++ src/screens/AuthScreens.jsx
@@ -1,4 +1,4 @@
-//ok it is at top of all  ssjsjsjsjsjsjsj
+//ok it is at top of all  ssjsjsjsjsjsjs
 import React, { useState, useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { AuthWrapper, Logo, Button, Input, StatusMessage, BackButton } from '../components/UI';
